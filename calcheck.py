6f5b5845250e9a3cdb98c086941524c4bf509a93
theniteshsingh <singhnitesh@live.com>
2020-01-28 11:02:30 +0530

    Create calcheck.py

---
+++ calcheck.py
@@ -0,0 +1,10 @@
+number1 = int(input("enter first number"))
+choice=input("+ for Add,2 for sub,- for multiply")
+if choice=="+":
+    Number2 = int(input("enter second number"))
+    Add=number1+Number2
+    print(Add)
+elif choice=="-":
+    Number2 = int(input("enter second number"))
+    Sub=number1-Number2
+    print(Sub)
